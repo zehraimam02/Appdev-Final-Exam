573f3976e452e901e70a3a5d9a37aa3d3d32dbbc
zehraimam02 <izehra70@gmail.com>
2024-12-27 15:50:35 +0500

    first commit

---
+++ finalexam_app/android/app/src/main/kotlin/com/example/finalexam_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finalexam_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ finalexam_app/lib/main.dart
@@ -0,0 +1,41 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'blocs/sample_bloc.dart';
+import 'firebase_options.dart';  
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => MyBloc(),
+      child: MaterialApp(
+        title: 'Final Exam App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
